a8f3b31b2a276e40238f8ba7b580a723946cf17c
AureGitHub <auredeveloper@gmail.com>
2024-04-09 11:05:47 +0200

    poder inicializar entities con valores (ej para las fk)

--- projects/app-ionic-base-lib/src/components/header/header.component.ts
+++ projects/app-ionic-base-lib/src/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { SeguridadService } from '../../services/seguridad.service';
 import { UtilService } from '../../services/util.service';
@@ -17,6 +17,9 @@ export class HeaderComponent  implements OnInit {
   appDescripcion='';
   currentmenuItem: any;
   lastMin = false;
+
+  @Input() iconUserVisible = true;
+
 
   constructor(
     private seguridadService: SeguridadService,
